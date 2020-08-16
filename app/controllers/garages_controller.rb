96e2e57d01d6c45044dde41994725f6e57b56b7d
Fiveolioo <96richardohc@gmail.com>
2020-08-16 00:08:28 -1000

    have GET /garages

--- app/controllers/garages_controller.rb
+++ app/controllers/garages_controller.rb
@@ -2,4 +2,16 @@ class GaragesController < ApplicationController
   get '/garages/new' do
     erb :'/garages/new'
   end
+
+  post '/garages' do
+    garage = Garage.create(
+      name: params[:name]
+    )
+    redirect "/garages/#{garage.id}"
+  end
+
+  get '/garages' do
+    @garages = Garage.all
+    erb :'/garages/index'
+  end
 end
