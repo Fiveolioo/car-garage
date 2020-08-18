8c95c89bad98a0ecccef785f6d8f8c4d1395d678
Fiveolioo <96richardohc@gmail.com>
2020-08-18 00:50:55 -1000

    cars edit page created

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,3 +1,15 @@
 class CarsController < ApplicationController
+  post '/cars' do
+    garage = Garage.find_by(id: params[:garage_id])
 
+    car = Car.new(
+      make: params[:make],
+      model: params[:model],
+      color: params[:color]
+    )
+    car.driver = current_user
+    car.garage = Garage.find(params[:garage_id])
+    car.save
+    redirect "/garages/#{car.garage_id}"
+  end
 end
